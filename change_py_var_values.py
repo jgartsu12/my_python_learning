7f6b43facfb246f5a1f41da8d63e1a453c39bde1
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 18:41:11 -0500

    changed data type can cause errors

--- change_py_var_values.py
+++ change_py_var_values.py
@@ -11,6 +11,6 @@ third = 'Alutve'
 
 print(second) # prints Bregman
 
-second ='Correa'
+second = 0.280
 
-print(second) # prints Correa
+print(second) # prints 0.28 ... warning tells u redefined str to float in real world app if u called method on second var but accident reset to float u will get error due to being the wrong data type == poor practice == bug
