896bf032842846a705b6ed0aba560005490735e9
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 18:38:37 -0500

    change second var to different name

---
+++ change_py_var_values.py
@@ -0,0 +1,16 @@
+# meal_completed = True
+# total = 100
+# tip = total * 1/5
+# total = total + tip
+# receipt = "Your total is " + str(total)
+# print(receipt)
+
+first = 'Springer'
+second = 'Bregman'
+third = 'Alutve'
+
+print(second) # prints Bregman
+
+second ='Correa'
+
+print(second) # prints Correa
