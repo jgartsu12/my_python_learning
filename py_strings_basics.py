87023846381fbf682d06de6da5cb58152a901f29
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 18:56:20 -0500

    single quotes vs double quotes

---
+++ py_strings_basics.py
@@ -0,0 +1,4 @@
+sentence = 'The quick brown fox jumped over the lazy dog' # this is common in dev cuz has every letter in alphabet
+
+sentence_two = 'That is my dog's bowl' #error syntax cuz dog's with single quotes must escape character by using back slash
+print(sentence)
