bf5cd311e69d445b6fd3649cb3da2119e65a04bd
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 18:29:46 -0500

    initial commit

---
+++ data_types.py
@@ -0,0 +1,20 @@
+meal_completed = True
+sub_total = 100
+# tip = sub_total * 0.2
+tip = sub_total * 1/5 # can use fractions as well
+total = sub_total + tip
+receipt = "Your total is " + str(total)
+print(receipt)
+# bill calculator for Overview of Python Data Types
+
+'''
+- Booleans
+- Numbers
+- Strings
+- Bytes and byte arrays
+- None
+- Lists
+- Tuples
+- Sets
+- Dictionaries
+'''
