0356017e0999cbb64b3754d91ae93d521c512f99
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 09:14:41 -0500

    \n and heredocs

--- build_raw_multiline_str.py
+++ build_raw_multiline_str.py
@@ -1,10 +1,14 @@
-content = """
-Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+# content = """
+# Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
 
-Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+# Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
 
-Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
-"""
+# Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+# """
 
-print(repr(content)) # prints ‘\n content \n’ |||| inclduing where this line breaks
+# print(repr(content)) # prints ‘\n content \n’ |||| inclduing where this line breaks
 
+
+long_string = '\nNullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.\n\nVestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.\n\nInteger posuere erat a ante venenatis dapibus posuere velit aliquet.\n'
+
+print(long_string) # this prints out extacly like the heredoc was in previous Herddoc lesson
