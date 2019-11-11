8821cbc960642bd5d2a680d2c469983ec10ea2a7
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 08:47:24 -0500

    working wit ranges wrong way

--- access_values_in_strings_ranges.py
+++ access_values_in_strings_ranges.py
@@ -1,3 +1,8 @@
+#working with ranges 
+# horrible way to use ranges this way not efficient
 starter_sentence = 'The quick brown fox jumped'
-starter_sentence[0]
-print (starter_sentence)
+first = starter_sentence[0]
+second = starter_sentence[1]
+third = starter_sentence[2]
+new_sentence = first + second +third
+print (new_sentence)
