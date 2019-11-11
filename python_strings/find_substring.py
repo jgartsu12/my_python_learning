cda244a6525349f73488e40d2d535b167fce8535
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:36:00 -0500

    in operator for True

--- python_strings/find_substring.py
+++ python_strings/find_substring.py
@@ -1,9 +1,7 @@
 sentence = 'The quick brown fox jumped over the lazy dog.'
-# index vs find method
-query = sentence.find('oops')
-# query_two = sentence.index('oops')
+# in operator most prefered by py develops
+query = 'fox' in sentence
 
-print(query) # prints -1 since find method cant find 'oops'; while index will print an error
-# print(query_two) 
+print(query) # prints True
 
 
