e851760ee9028542adcd0be2e206eb392d1eb444
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 11:02:18 -0500

    neg index -4

--- python_strings/neg_index.py
+++ python_strings/neg_index.py
@@ -1,5 +1,8 @@
 # Using a Negative Index with a Python String
 sentence = 'The quick brown fox jumped over the lazy dog.'
 
-print(sentence[-1])
-# prints period .
+print(sentence[-4:])
+# prints  dog.
+# goes from -4 to rest of string from dog
+
+# machine learning
