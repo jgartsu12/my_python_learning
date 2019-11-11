749aadef17381776a0570d41d7e853b70e316b27
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 08:56:20 -0500

    generated new sentence using ranges

--- access_values_in_strings_ranges.py
+++ access_values_in_strings_ranges.py
@@ -1,6 +1,7 @@
-#working with ranges 
-# [first element : not including this last element index value]
+# switching The to Thy and pulling in the rest of the starter_sentence
+
 starter_sentence = 'The quick brown fox jumped'
-first_word = starter_sentence[0:2]
-new_sentence = first_word
-print (new_sentence) # prints Th
+
+new_sentence = 'Thy' + starter_sentence[3:]
+# leaving blank after colon tells python u want to take range all the way to send
+print(new_sentence) #prints 
