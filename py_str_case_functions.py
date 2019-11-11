ad161c614f59f3951309f7c67bd9ad8ccd3bfdf8
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 19:18:04 -0500

    processes

--- py_str_case_functions.py
+++ py_str_case_functions.py
@@ -1,5 +1,20 @@
-sentence = 'The quick brown fox jumped' # str obj
+# stored str inside a sentence is a few processes 
+# first we have sentence that is a variable; then we have str which is 'THe quick ..' 
+# = is the variable assignment
+# 3 process; str, assignment; variable storing the str
+#sentence.upper() doesnt change str itself or variable cuz doesnt permanly make change just on the variable
+# data flow - when processes occur .. why somethign not changing? calling a process but not storing in variable or on obj made
+# when want to change values create new var
 
-sentence.upper() # only occurs when called
+#sentence = 'The quick brown fox jumped'
+# sentence --> var
+# 'The quick brown fox jumped' -> string
+# = -> assignment
 
-print(sentence)
+sentence = 'The quick brown fox jumped'
+sentence_two = sentence.upper()
+
+print(sentence) 
+print(sentence_two)
+# havent changed initial str value but have with other str value (sentence_two)
+# if change permantly other methods will call accidently and cause bugs
