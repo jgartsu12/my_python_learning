8226cd4ea4b637ba352d6d669a21e7fcdfefccde
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 19:00:12 -0500

    good time to use double quotes

--- py_strings_basics.py
+++ py_strings_basics.py
@@ -1,4 +1,7 @@
 sentence = 'The quick brown fox jumped over the lazy dog' # this is common in dev cuz has every letter in alphabet
 
-sentence_two = 'That is my dog's bowl' #error syntax cuz dog's with single quotes must escape character by using back slash
-print(sentence)
+sentence_two = 'That is my dog\'s bowl' # syntax error  cuz dog's with single quotes must escape character by using back slash
+                                        # \ includes apostphe instead of ending the line could use double quotes instead
+sentence_three = "That is my dog's bowl" # this prevents use of back slash \
+
+print(sentence_two)
