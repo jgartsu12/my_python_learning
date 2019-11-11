84fa9737c3b2edb5a5afcbacbee02f1eacf4bdea
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 09:49:38 -0500

    using str interpolating with heredoc

--- modern_string_interpolation.py
+++ modern_string_interpolation.py
@@ -1,4 +1,22 @@
 name = 'Seb'
+product = 'Python e course'
+# f formats to run python code inside of email_content
+email_content= f""" 
+Hi {name}
 
-greeting = f'Hi {{bracket}}'
-print(greeting) # prints Hi 4 in terminal
+Thank you for purchasing {product}
+
+Regards,
+
+Sales Team
+"""
+
+print(email_content) 
+# prints : 
+# Hi Seb
+
+# Thank you for purchasing Python e course
+
+# Regards,
+
+# Sales Team
