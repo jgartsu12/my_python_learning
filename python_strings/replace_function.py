627ca1e5d945f68fc71ff871dfe8dac335abfe84
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:50:16 -0500

    immutability with strings explained

---
+++ python_strings/replace_function.py
@@ -0,0 +1,7 @@
+# Using Python's replace Function to Find and Replace String Values
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+sentence = 'New Value'
+
+print(sentence)
+# prints New Value since not touching str but replacing what is stored in sentence variable --> IT DID NOT CHANGE STR sinces strs are immutable
