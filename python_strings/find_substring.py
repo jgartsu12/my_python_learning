21f6e79190f9b13053e98e65441c1f9d5b68b7e8
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:33:19 -0500

    explain diff between index vs find method

--- python_strings/find_substring.py
+++ python_strings/find_substring.py
@@ -1,11 +1,9 @@
 sentence = 'The quick brown fox jumped over the lazy dog.'
 # index vs find method
 query = sentence.find('oops')
-query_two = sentence.index('oops')
+# query_two = sentence.index('oops')
 
-print(query)
-print(query_two) 
-# prints error msg: Traceback (most recent call last):
- # File "find_substring.py", line 4, in <module>
-    # query_two = sentence.index('oops')
+print(query) # prints -1 since find method cant find 'oops'; while index will print an error
+# print(query_two) 
+
 
