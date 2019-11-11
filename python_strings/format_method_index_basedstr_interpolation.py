984c33b0e1685ed50f9483626ab9b7c97f4b79f9
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:07:08 -0500

    order of .format() doesnt matter notes

--- python_strings/format_method_index_basedstr_interpolation.py
+++ python_strings/format_method_index_basedstr_interpolation.py
@@ -3,7 +3,7 @@ name = 'Seb'
 age = 12
 product = 'Python eLearning Course'
 
-greeting = "Hi {0}, you are listed as {1} years old and you have purchased: {2} ...".format(name, age, product)
-
+greeting = "Product Purchase: {2} - Hi {0}, you are listed as {1} years old and you have purchased: {2} - {3}".format(name, age, product, 'John')
+# order doesnt matter just has to be mapped to right index value in .format(argument0...)
 print(greeting)
-# prints : Hi Seb, you are listed as 12 years old and you have purchased: Python eLearning Course ...
+# prints : Product Purchase: Python eLearning Course - Hi Seb, you are listed as 12 years old and you have purchased: Python eLearning Course - John
