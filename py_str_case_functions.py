9654677371dae60812dd463b375adff72e1f8588
jgartsu12 <jgartsu12@gmail.com>
2019-11-10 19:06:35 -0500

    use str.upper() fn

---
+++ py_str_case_functions.py
@@ -0,0 +1,3 @@
+sentence = 'The quick brown fox jumped'.upper() # call method and functions on objects
+                                                # convert entrie str to uppercase
+print(sentence)
