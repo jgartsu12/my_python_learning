0af5c0f68da311b446bc79caddd39b2b283038a1
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 09:15:12 -0500

    new line chars

--- build_raw_multiline_str.py
+++ build_raw_multiline_str.py
@@ -11,4 +11,4 @@
 
 long_string = '\nNullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.\n\nVestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.\n\nInteger posuere erat a ante venenatis dapibus posuere velit aliquet.\n'
 
-print(long_string) # this prints out extacly like the heredoc was in previous Herddoc lesson
+print(long_string) # this prints out extacly like the heredoc was in previous Herddoc lesson w/ new line chars
