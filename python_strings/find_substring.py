fc7ae80003e6eafb72c8d3327457ae4033a4efa3
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:28:22 -0500

    error msg

--- python_strings/find_substring.py
+++ python_strings/find_substring.py
@@ -1,11 +1,11 @@
 sentence = 'The quick brown fox jumped over the lazy dog.'
-# index method
-query = sentence.index('quick')
-query_two = sentence.index('quick')
+# index vs find method
+query = sentence.find('oops')
+query_two = sentence.index('oops')
 
-print(query) # prints 4 again ... difference --> algorithms ... index throughs index if it cant find value where find will do -1
+print(query)
 print(query_two) 
-
-# in method
-# why use one method over the other
+# prints error msg: Traceback (most recent call last):
+ # File "find_substring.py", line 4, in <module>
+    # query_two = sentence.index('oops')
 
