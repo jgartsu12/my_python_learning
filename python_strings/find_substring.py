7f55ef2b24d6048ebc31d4a3be144a3aa0003dea
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:19:36 -0500

    find method

--- python_strings/find_substring.py
+++ python_strings/find_substring.py
@@ -1,1 +1,8 @@
 sentence = 'The quick brown fox jumped over the lazy dog.'
+# find method
+query = sentence.find('quick')
+print(query) # prints 4 cuz quick is the fourth index in the str in sentence variable
+# index
+# in method
+# why use one method over the other
+
