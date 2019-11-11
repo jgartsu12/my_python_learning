c33afdeca30f5fa23eccbc6a1863c1f10826d7cf
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 08:50:33 -0500

    when range stops [start: stops at this value of index]

--- access_values_in_strings_ranges.py
+++ access_values_in_strings_ranges.py
@@ -1,6 +1,6 @@
 #working with ranges 
 # [first element : not including this last element index value]
 starter_sentence = 'The quick brown fox jumped'
-first_word = starter_sentence[0:3]
+first_word = starter_sentence[0:2]
 new_sentence = first_word
-print (new_sentence) # prints The
+print (new_sentence) # prints Th
