a25acaf3bb626fb5531af5ae25578e0b9b9051e8
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:21:24 -0500

    substring

--- python_strings/find_substring.py
+++ python_strings/find_substring.py
@@ -1,7 +1,7 @@
 sentence = 'The quick brown fox jumped over the lazy dog.'
 # find method
-query = sentence.find('quick')
-print(query) # prints 4 cuz quick is the fourth index in the str in sentence variable
+query = sentence.find('qui')
+print(query) # prints 4 cuz qui is in quick which is in the fourth index in the str in sentence variable ... matches letters in the string in query called a substring
 # index
 # in method
 # why use one method over the other
