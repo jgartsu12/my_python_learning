c885f26e896cc6b525b75852cc5b9d2114be1f38
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 08:44:47 -0500

    cant change str

--- access_values_in_strings_ranges.py
+++ access_values_in_strings_ranges.py
@@ -1,8 +1,3 @@
-# 'The quick brown fox jumped'
-# index - zero-based numbering sys
-# T => 0 is index
-# h => 1
-# e => 2
-# ' ' => 3 # empty space is an index
 starter_sentence = 'The quick brown fox jumped'
-print (starter_sentence[0])
+starter_sentence[0]
+print (starter_sentence)
