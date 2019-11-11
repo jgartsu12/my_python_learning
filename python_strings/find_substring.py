2d0955de57d1447e3f91ef843f1558ebd67e6a71
jgartsu12 <jgartsu12@gmail.com>
2019-11-11 10:43:28 -0500

    find vs index vs in operator and applied to py conditionals

--- python_strings/find_substring.py
+++ python_strings/find_substring.py
@@ -4,4 +4,12 @@ query = 'oops' in sentence
 
 print(query) # prints False
 
-
+# find - index value where discovered first
+# index - index value where discovered can cause program to stop if try to run and website breaks if user went to it 
+# in operator - doesnt care about index value ... prefered to see if a value is in the string True or False in case u need to replace something .. does string contain this substring
+    # in operator --> in conditionals:
+if 'oops' in sentence:
+    ... # cleaner code cuz less code
+    # compared to unclean
+if (sentence.finds('oops') != -1): 
+    ... # less clean in conditionals will run into this but just showing u ... how to follow ind best practice --> USE In operator syntax
