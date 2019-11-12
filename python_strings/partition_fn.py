a51f2e75a8f91eddce5769cea1568a181f754a45
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:16:19 -0500

    how parition always returns 3 elements

--- python_strings/partition_fn.py
+++ python_strings/partition_fn.py
@@ -1,10 +1,10 @@
 # Guide to Python's Partition Function
-heading = "Python: An Introduction"
+heading = "Python: An Introduction, and Python: Advanced"
 
 first, second, third = heading.partition(': ')
             # prints same as previous commit
 print(first) # prints Python
 print(second) # prints :
-print(third)  # prints An Introduction
+print(third)  # prints An Introduction, and Python: Advanced
 # NOTES:
-# parition breaks what ever value u pass in and return 3 elements doesnt matter how big string is .. sentinal value?
+# always returns 3 elements
