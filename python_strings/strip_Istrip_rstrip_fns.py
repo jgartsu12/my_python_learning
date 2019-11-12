dd88384ac6bbe7c96b842d803e90018602cb8ee3
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 08:50:37 -0500

    L strip

--- python_strings/strip_Istrip_rstrip_fns.py
+++ python_strings/strip_Istrip_rstrip_fns.py
@@ -1,4 +1,8 @@
 # Overview of Python's strip, lstrip (L strip), and rstrip Functions
 url = '     https://google.com      '
 
-print(url.strip('https://')) # returns google.com  # error suspicous argument in strip call - dont want users to use these strip calls cuz they ccould inject something that could impact other data - security 
+print(url.strip('https://'))
+
+# using L - strip => Left strip
+print(url.lstrip('htts://'))
+    # pirnts google.com
