5434eb7579de919b7b32969394b8a32ab643deb8
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 10:15:08 -0500

    floor division rounds down

--- python_numbers/math_operators.py
+++ python_numbers/math_operators.py
@@ -8,11 +8,11 @@ print(100 - 42.3) # prints 57.7;; integer and floats can work together
 print('Division')
 print(100 / 42.3)  # prints 2.3640661938853281
 print(100 / 42)  # prints 2.380952380952381
-print(100 / 38)
+print(100 / 38)   # prints 2.631....
 
 print('Floor Division')
-print(100 // 42)
-print(100 // 38)
+print(100 // 42) # prints 2 rounds down automatically for us \\    ** used for approx value that is an integer... not accurate for complex calculations
+print(100 // 38) # prints 2 rounds down to nearest whole number not like normal rounding
 
 print('Multiplication')
 print(100 * 42)
