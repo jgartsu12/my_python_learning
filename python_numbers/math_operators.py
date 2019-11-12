914520c1d041b9d25e34f4de23b855cee0566fad
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 10:12:08 -0500

    division

--- python_numbers/math_operators.py
+++ python_numbers/math_operators.py
@@ -6,7 +6,8 @@ print('Subtraction')
 print(100 - 42.3) # prints 57.7;; integer and floats can work together
 
 print('Division')
-print(100 / 42)
+print(100 / 42.3)  # prints 2.3640661938853281
+print(100 / 42)  # prints 2.380952380952381
 print(100 / 38)
 
 print('Floor Division')
