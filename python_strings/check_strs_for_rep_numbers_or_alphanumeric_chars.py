38ba567b41816cf5d449acf210545e0ec5a7e5f2
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:46:44 -0500

    why .isalpha() not used as often

--- python_strings/check_strs_for_rep_numbers_or_alphanumeric_chars.py
+++ python_strings/check_strs_for_rep_numbers_or_alphanumeric_chars.py
@@ -1,10 +1,5 @@
 # How to Check if Strings Represent Numbers or Alphanumeric Characters in Python
 api_data = '5' # string with 5 inside
-greeting = 'Hi '
+greeting = 'Hi there'
 
-# isnumeric() fn
-print(api_data.isnumeric()) # prints True
-print(greeting.isnumeric()) # prints False
-#Notes
-# 5 is a string with a number thus it is numeric so its True
-# use isnumeric() in db query to get integer instead of false positives
+print(greeting.isalpha()) # prints false since the space in middle between hi and there is not alphanumeric; 100% needs to alphanumeric why not used as often
