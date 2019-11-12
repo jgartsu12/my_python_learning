c203c6a3ab7726c4cb712ab8dbfc63e7e3881a5f
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:05:00 -0500

    using parititions notes

---
+++ python_strings/partition_fn.py
@@ -0,0 +1,12 @@
+# Guide to Python's Partition Function
+heading = "Python: An Introduction"
+header, _, subheader = heading.partition(': ')
+
+print(header) # prints Python
+print(subheader) # prints An Introduction
+# NOTES:
+# intro to collections in py and strings can be conencted and generate their own collections
+#heading = python ; subheading = An Introduction - how to sep w/ partition
+#partition takes in an argument which is the divider and then partitions entire string into the 3 elements
+    # parition performs a tuple collection breaks into 3 objects to perform VARIABLE DESCONSTRUCTION
+    # _, is best practice
