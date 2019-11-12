9860cdc21a1c34dbb72f2333fadb9f9475d62f3b
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 10:55:50 -0500

    float values

---
+++ python_numbers/decimal_vs_float.py
@@ -0,0 +1,8 @@
+product_cost = 88.40 # python interpreter ignores trailing 0's
+commission_rate = 0.08
+qty = 450
+
+product_cost += (commission_rate * product_cost)
+print(product_cost * qty) # prints 42962.4 float value
+# import outside libaries
+# decimal --> advanced calculations that need to be used for preccison
