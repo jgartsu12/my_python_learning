32462399f252b4617c6f0f9f5c452d6f7409d7b5
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 11:02:05 -0500

    python decimal library

--- python_numbers/decimal_vs_float.py
+++ python_numbers/decimal_vs_float.py
@@ -1,8 +1,9 @@
-product_cost = 88.40 # python interpreter ignores trailing 0's
-commission_rate = 0.08
+from decimal import Decimal # import decimal python library # take Decimal class from decimal library
+
+product_cost = Decimal(88.40) # python decimals
+commission_rate = Decimal(0.08)
 qty = 450
 
 product_cost += (commission_rate * product_cost)
-print(product_cost * qty) # prints 42962.4 float value
-# import outside libaries
-# decimal --> advanced calculations that need to be used for preccison
+print(product_cost * qty) # prints 42962.40000000000282883716451 
+# decimals give u higher level of precision 
