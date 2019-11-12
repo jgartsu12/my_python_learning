9318da2b7c1a8746a8d616b9498b63a0bc86febc
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 11:20:15 -0500

    complex()

--- python_numbers/convert_btwn_number_data_types.py
+++ python_numbers/convert_btwn_number_data_types.py
@@ -9,3 +9,4 @@ qty = 450
 print(int(product_cost)) # prints 88  - keeps whatever int value is there
 print(float(qty)) # prints 450.0
 print(Decimal(product_cost)) # convert to decimal --> 88.799999999999999999999715...
+print(complex(commission_rate)) # scientific prints (0.08+0j) - a complex obj
