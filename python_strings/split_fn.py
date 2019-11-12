ed9374f050e8f1c783e93f4089915c6fe5a515cf
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:31:20 -0500

    split vs parition()

--- python_strings/split_fn.py
+++ python_strings/split_fn.py
@@ -1,9 +1,6 @@
 # split fn - used qd devmnt - single delimator - seperates str into 2 parts
 # split vs partition
-tags = 'python,coding,programming,development'
+heading = "Python: An Introduction"
 
-# tags = tags.split() poor practice - split returns a list of elements => bugs 
-list_of_tags = tags.split()
-print(list_of_tags) # prints ['python,coding,programming,development']
-# now u can work with this data as a collection
-# split without default converts u string into a single element in a list 
+heading_list = heading.split(': ')
+print(heading_list) # prints --> ['Python', 'An Introduction']
