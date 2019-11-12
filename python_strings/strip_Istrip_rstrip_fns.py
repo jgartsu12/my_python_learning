dc954a5994df4d4a9768ac039b6fbe05a99b7e1b
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 08:47:06 -0500

    securityand suspicous arg error in strip()

--- python_strings/strip_Istrip_rstrip_fns.py
+++ python_strings/strip_Istrip_rstrip_fns.py
@@ -1,4 +1,4 @@
 # Overview of Python's strip, lstrip (L strip), and rstrip Functions
 url = '     https://google.com      '
 
-print(url.strip('https://')) # returns google.com 
+print(url.strip('https://')) # returns google.com  # error suspicous argument in strip call - dont want users to use these strip calls cuz they ccould inject something that could impact other data - security 
