ec4d4a8d42b1b0883bd5b53a12ff90606a8448a3
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 10:00:11 -0500

    how number data types can be used together

--- python_numbers/number_types.py
+++ python_numbers/number_types.py
@@ -1,5 +1,8 @@
 product_id = 123 # integer data type -> whole number (not decimal, fraction, nor complex number)
 sale_price = 14.99 # float -> precision isnt near as granular as a decimal .. not for science 
 tip_percentage = 1/5 # fraction represents 0.2 or 20%
+new_product = 150  
 
-print(tip_percentage) # prints 0.2 # python manages number type by itself
+print(sales_price + new_product) # prints 164.99 # python manages number type by itself
+# combine for calculation to switch between number types very flexible
+# can take two diff types of number data types and converts final output into a float
