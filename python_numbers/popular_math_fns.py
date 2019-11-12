c4a4c7d23d5f0bc00a1b4e9d536830214889f327
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 11:24:46 -0500

    abs()

---
+++ python_numbers/popular_math_fns.py
@@ -0,0 +1,7 @@
+import math 
+
+loss = -20.25
+product_cost = 89.99
+#abs - absolute value - removes neg sign
+print(abs(loss)) # prints 20.25
+# automate processes talked aabout and call them on numbes via math fns
