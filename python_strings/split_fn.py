7b89b71ddee7fb8efbcc8518260fa9cc723f5e19
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:35:08 -0500

    key diff btwn split and partition

--- python_strings/split_fn.py
+++ python_strings/split_fn.py
@@ -4,3 +4,9 @@ heading = "Python: An Introduction"
 
 heading_list = heading.split(': ')
 print(heading_list) # prints --> ['Python', 'An Introduction']
+# partition returns a tuple; split returns a list 
+    # key diff --> very easy to perform variable deconstruction (done with partition ... tuple)
+        ## API structure used easily with tuples to var deconstruct it
+    # split more flexible works with wider array of strings
+        # split allows u to break up string into independent elements in a list
+        # split used most of the time 
