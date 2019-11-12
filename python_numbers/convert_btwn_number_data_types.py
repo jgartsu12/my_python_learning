24e3ccb900e08274d2a24445d203dc61202d925f
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 11:12:06 -0500

    py default converts to float

---
+++ python_numbers/convert_btwn_number_data_types.py
@@ -0,0 +1,7 @@
+# How to Convert Between the Integer, Float, Decimal and Complex Numeric Data Types in Python
+product_cost = 88.40
+commission_rate = 0.08
+qty = 450
+
+print(product_cost * qty)  # this will automatically convert into a float 
+# prints 39780.0 a float (py default converts to float)
