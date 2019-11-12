4f1c645f279d28eabc3e91cbdac51cd15ce1fed7
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:57:26 -0500

    fraction

--- python_numbers/number_types.py
+++ python_numbers/number_types.py
@@ -2,4 +2,4 @@ product_id = 123 # integer data type -> whole number (not decimal, fraction, nor
 sale_price = 14.99 # float -> precision isnt near as granular as a decimal .. not for science 
 tip_percentage = 1/5 # fraction represents 0.2 or 20%
 
-print(tip_percentage) # prints 0.2 # python manages numeber type by itself
+print(tip_percentage) # prints 0.2 # python manages number type by itself
