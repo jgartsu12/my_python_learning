392cfedf190b540d1be587d701896ca4a1e850c8
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 11:27:31 -0500

    math.floor

--- python_numbers/popular_math_fns.py
+++ python_numbers/popular_math_fns.py
@@ -2,6 +2,9 @@ import math
 
 loss = -20.25
 product_cost = 89.99
-#abs - absolute value - removes neg sign
+#abs - absolute value - removes neg sign - import math not needed it is in python core library
 print(abs(loss)) # prints 20.25
 # automate processes talked aabout and call them on numbes via math fns
+
+print(math.floor(product_cost)) # imported from math library
+#prints 89 - printed floor - could have just called int()
