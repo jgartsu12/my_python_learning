da6d772775f2098ae3c3e343570e4733c7295916
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:41:08 -0500

    using isalpha()

---
+++ python_strings/check_strs_for_rep_numbers_or_alphanumeric_chars.py
@@ -0,0 +1,12 @@
+# How to Check if Strings Represent Numbers or Alphanumeric Characters in Python
+api_data = '5' # string with 5 inside
+greeting = 'Hi '
+
+print(api_data.isalpha()) # prints False
+print(greeting.isalpha()) # prints True
+#Notes
+# focus on APIs data to check for interdata between api responses
+# API data comes from string format mostly
+#working with str data
+# working w/ api's u will get response and u will need to be a db query ... db needs id with an integer
+# isalpha () - chekcs for alpha numeric in the string - checking to see what the value is
