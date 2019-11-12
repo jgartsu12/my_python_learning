e2515c0adf94a8d5e96fdc3118ad3077fa9eeb26
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 09:06:59 -0500

    purpose of partition fn

--- python_strings/partition_fn.py
+++ python_strings/partition_fn.py
@@ -5,8 +5,4 @@ header, _, subheader = heading.partition(': ')
 print(header) # prints Python
 print(subheader) # prints An Introduction
 # NOTES:
-# intro to collections in py and strings can be conencted and generate their own collections
-#heading = python ; subheading = An Introduction - how to sep w/ partition
-#partition takes in an argument which is the divider and then partitions entire string into the 3 elements
-    # parition performs a tuple collection breaks into 3 objects to perform VARIABLE DESCONSTRUCTION
-    # _, is best practice
+# want to get rid of ': ' purpose of partition adn the reassignmnt
