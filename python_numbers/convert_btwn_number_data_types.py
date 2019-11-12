d07fb9b749b984c9c9635fe239604b582992d06d
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 11:19:00 -0500

    decimal conversion

--- python_numbers/convert_btwn_number_data_types.py
+++ python_numbers/convert_btwn_number_data_types.py
@@ -1,8 +1,11 @@
 # How to Convert Between the Integer, Float, Decimal and Complex Numeric Data Types in Python
+from decimal from Decimal 
+
+
 product_cost = 88.40
 commission_rate = 0.08
 qty = 450
 
-
-print(int(product_cost) * qty)) # prints 88  - keeps whatever int value is there
+print(int(product_cost)) # prints 88  - keeps whatever int value is there
 print(float(qty)) # prints 450.0
+print(Decimal(product_cost)) # convert to decimal --> 88.799999999999999999999715...
