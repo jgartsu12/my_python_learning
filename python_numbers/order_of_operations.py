7b1401461c7a3e8e60469fb007f24f2eb44e4c75
jgartsu12 <jgartsu12@gmail.com>
2019-11-12 10:34:24 -0500

    assignmnt op

--- python_numbers/order_of_operations.py
+++ python_numbers/order_of_operations.py
@@ -1,3 +1,15 @@
 calculation = 8 + 2 * 5 - (9 + 2) ** 2
 
-print(calculation)
+print(calculation) #prints  -103
+
+"""   notes:
+ ~~~~~~~~~~~~~~~~~~
+ P - Parenthesis () first to get processses which is (9+2)
+ E - Exponents ** 2
+ ~~~~~~~~~~~ (M and D go right to left and doesnt matter order)
+ M- Multiplication 
+ D - Division
+ ~~~~~~~~~~ (A and S same as M and D)
+ A - Addition
+ S - Subtraction
+ """
