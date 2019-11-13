f7b0700f4b030e762788b0b43fdab0cf601dfc24
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:04:23 -0500

    math.pow vs ** op

--- python_numbers/popular_math_fns.py
+++ python_numbers/popular_math_fns.py
@@ -2,6 +2,9 @@ import math
 
 loss = -20.25
 product_cost = 89.99
-# math.sqrt fn
-        # sqrt gets square root 
-print(math.sqrt(product_cost)) # prints  9.486305...
+# math.pow fn
+        # pow gets power of number takes in 2 arguments
+        # pow is much more scientifically accurate used in science
+        # basic ** operator when not needing to be super accurte
+print(math.pow(5, 2)) # prints  25.0 ... => 5**2 ... pow prints float
+print(5**2) # same as above but print 25 ... ** prints integer
