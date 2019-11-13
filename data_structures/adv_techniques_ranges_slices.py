073fa05b04fe9f9da62f6d9458650e1c296fe37a
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:57:09 -0500

    1:-1

---
+++ data_structures/adv_techniques_ranges_slices.py
@@ -0,0 +1,13 @@
+# Advanced Techniques for Implementing Ranges and Slices in Python Lists
+# ranges vs slices 
+    # ranges usually used when working w/ strings but slices in lists (some used interchangably)
+tags = [
+  'python',
+  'development',
+  'tutorials',
+  'code',
+  'programming',
+  'computer science'
+]
+
+tag_range = tags[1:-1] # going to start with second element with dvp and go all the way up to comp sci and not include it
