3c52645cdf311d08296d060bef3b908c876be795
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:14:09 -0500

    sort alphabetically with sort() fn

---
+++ data_structures/sort_lists.py
@@ -0,0 +1,9 @@
+# How to Sort Lists [] in Python
+tags = ['py', 'dvp', 'tutorials', 'code']
+
+print(tags) # prints in exact order of their index what if u want to sort by their alphabetical value
+# lists are muttable
+# use sort() to alphabetize list so not in the index order
+tags.sort()
+
+print(tags) # printed ['code', 'dvp', 'py', 'tutorial'] -- list sort() default in alphabetic order
