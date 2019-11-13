8e2c99c88231b5139774ce549b6a9b79e097efeb
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:23:56 -0500

    perform basic reassignmnt

--- python_strings/data_structures/py_lists.py
+++ python_strings/data_structures/py_lists.py
@@ -10,8 +10,7 @@ users = ['John', 'Seb', 'Ken']
 # strings are immutable but lists muttable - u can change lists
 print(users) # prints the user with true data structure -- list of str names
 
-#query specific elemnts inside of list - how to get access to the list
-print(users[2])
-    # prints Tiffany w/o [ ]; thus is no longer a list object; returns string object Tiffany
-
-print([users[2]]) # prints ['Tiffany']
+# edit the list
+    # perform standard assignment - find element with index of four and assign t his value
+users[1] = 'Jan'
+print(users) # prints ['John', 'Jan', 'Ken']
