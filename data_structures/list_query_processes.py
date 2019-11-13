7f800a1d4276a8409803481cae8ec6d906c7e8a0
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:02:29 -0500

    intro to len()

---
+++ data_structures/list_query_processes.py
@@ -0,0 +1,8 @@
+# Overview of Python List Query Processes: len(), Negative Indexes, and index()
+
+# length fn = len() - count of elements in a list .. inside ur query ?
+tags = ['python', ' development', 'tutorials', 'code']
+
+number_of_tags = len(tags)
+
+print(number_of_tags) # prints 4 --> 4 elements inside of number_of_tags list
