35ffed5ad9c182b510e5b9bc3870781debf825bc
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:17:24 -0500

    sort(reverse=True) w/ alphanumeric characters

--- data_structures/sort_lists.py
+++ data_structures/sort_lists.py
@@ -3,7 +3,7 @@ tags = ['py', 'dvp', 'tutorials', 'code']
 
 print(tags) # prints in exact order of their index what if u want to sort by their alphabetical value
 # lists are muttable
-# use sort() to alphabetize list so not in the index order
-tags.sort()
+# use sort() to sort backwards; default with dates sorts oldest firsts and then newests
+tags.sort(reverse=True)
 
-print(tags) # printed ['code', 'dvp', 'py', 'tutorial'] -- list sort() default in alphabetic order
+print(tags) # printed ['tutorials', 'py', 'dvp', 'code'] -- list sort(reverse=True) in reverse-alphabetical order
