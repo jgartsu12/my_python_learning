8a791f2bec929253608d8c2e809f686fa418c8c7
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:47:18 -0500

    can mix data types in same list

--- data_structures/nested_lists_store_multiple_data_types.py
+++ data_structures/nested_lists_store_multiple_data_types.py
@@ -1,1 +1,8 @@
 # Guide to Nested Lists and Best Practices for Storing Multiple Data Types in a Python List
+users = ['K', 'T', 'J', 'L']
+
+ids = [ 1, 2, 3, 4]
+
+mixed_list = [42, 32.2, 'Alutve']
+
+print(mixed_list) # prints mixed list like normal lists with mixed data types
