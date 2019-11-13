e754eb173d5e0d943217f623b9dbc816e1a973c7
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:29:58 -0500

    remove()

--- python_strings/data_structures/remove_items_list.py
+++ python_strings/data_structures/remove_items_list.py
@@ -1,1 +1,10 @@
 # Three Ways to Remove Elements from a Python List
+users = ['J', 'K', 'L', 'M']
+
+print(users)
+
+#first method : remove() fn
+    # remove('str wanted to remove')
+users.remove('J')
+
+print(users) # prints ['K', 'L', 'M']
