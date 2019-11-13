dc09bca7b675ed9f8581d3ff4dc02621a1c77d9f
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:14:55 -0500

    insert()

--- python_strings/data_structures/py_lists.py
+++ python_strings/data_structures/py_lists.py
@@ -7,5 +7,9 @@ Ken
 """
 
 users = ['John', 'Seb', 'Ken']
-
+# strings are immutable but lists muttable - u can change lists
 print(users) # prints the user with true data structure -- list of str names
+
+#place new element into the list #insert (index_for_placement_value, "inserrted str")
+users.insert(0, 'Anthony') 
+print(users)  #makes list 4 elements with Anthony before John
