70b45e19e6be593850e142ad9092e3ab583cf946
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:04:09 -0500

    off by one error

--- data_structures/list_query_processes.py
+++ data_structures/list_query_processes.py
@@ -5,4 +5,7 @@ tags = ['python', ' development', 'tutorials', 'code']
 
 number_of_tags = len(tags)
 
-print(number_of_tags) # prints 4 --> 4 elements inside of number_of_tags list
+print(number_of_tags) 
+
+# len() vs index() 
+    # 4 elements are inside but the it still starts with index 0 so last index value in this list is 3 not four == error: off-by 1-error
