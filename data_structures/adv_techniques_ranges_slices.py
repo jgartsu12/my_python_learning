7af0de05556f0ad4217705690e8a2fc77c6d0c1b
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 11:15:58 -0500

    sort vs slice method and immutability with sort

--- data_structures/adv_techniques_ranges_slices.py
+++ data_structures/adv_techniques_ranges_slices.py
@@ -9,7 +9,22 @@ tags = [
   'programming',
   'computer science'
 ]
-# want to reverse all these index values: how - use slice tech
-tag_range = tags[::-1] # flip entire order of the list
+# how is slice method different from sort() ???
+# key difference: 
+       # slice - it just reversed the index value
+       # sort() - it looks at alphabetical value and reversed that
+# tag_range = tags[::-1]
 
-print(tag_range) # prints ['computer science', 'programming', 'code', 'tutorials', 'development', 'python']
+ # print(tag_range) # prints ['computer science', 'programming', 'code', 'tutorials', 'development', 'python']
+
+#sort method
+tags.sort(reverse=True)
+print(tags) # prints ['tutorials','python', 'programming', 'development', 'computer science', 'code']
+
+# how sort method can be tricky
+sorted_tags = tags.sort(reverse=True)
+print(sorted_tags) # prints None ... python's view on IMMUTABILITY --> what u can and cannot change
+                    # python is so careful with immutability to so sort returns nothing --> changed order of the tags and reverse them but it will not return that value 
+                        # key -> sort fn goes in and changes tags and but does not store it as an operation in a variable due to immutablity
+                            # give warning that u changed an entire sort of elements to tell u tags list was effected
+                            
