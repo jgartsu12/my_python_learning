d16a1428742ddc1b703ad80550a4bf88ec5d780a
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:08:32 -0500

    how to traverse through elements in a list

--- data_structures/list_query_processes.py
+++ data_structures/list_query_processes.py
@@ -7,6 +7,9 @@ number_of_tags = len(tags)
 # how can i grab w/o knowing the index?? 
 last_item = tags[-1] # get last element by passing -1 to grab last element  to grab 'code' which is also index 3
 
+# u can also  index() fn
+index_of_last_item =tags.index(last_item)
+
 print(number_of_tags) 
 print(last_item) # prints code since it is the last item at index -1 or 3
-
+print(index_of_last_item) # prints index of 3 which is code
