777d9ba34a090b5a9f5cd584f834631e01bc9537
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:45:34 -0500

    range 1:3

--- data_structures/ranges_lists.py
+++ data_structures/ranges_lists.py
@@ -2,6 +2,6 @@
     # list of tags w/ 4 elements
 tags = ['pyton', 'development', 'tutorials', 'code']
 
-tag_range = tags[1:2] # dvp is index 1; tutorials is index 2; stops at index 2
+tag_range = tags[1:3] # dvp is index 1; tutorials is index 2; stops at index 3
 
-print(tag_range) # prints ['development'] 
+print(tag_range) # prints ['development', 'tutorials'] 
