da5826135ce31993d432442aaca330d6b567b406
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 08:41:56 -0500

    ceil()

--- python_numbers/popular_math_fns.py
+++ python_numbers/popular_math_fns.py
@@ -2,11 +2,7 @@ import math
 
 loss = -20.25
 product_cost = 89.99
-#abs - absolute value - removes neg sign - import math not needed it is in python core library
-print(abs(loss)) # prints 20.25
-# automate processes talked aabout and call them on numbes via math fns
-
-print(math.floor(product_cost)) 
-# could have just called int() --> may not do cuz floor clearly shows ur intention --> explicit intentions w/ floor
-        # math floor --> wanted rounded down lower value of product_cost ... int() --> intention to convert to an integer
-        
+# contrapositive of math.floor is ceil()
+print(math.ceil(product_cost))
+# prints 90 
+# ceiling prints rounded up value -- always rounds up to neartest whole number integer
