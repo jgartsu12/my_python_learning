5850b5391057c554c00785da7b17c2f634bcd1c7
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:54:29 -0500

    fixed pop notes

--- data_structures/nested_lists_store_multiple_data_types.py
+++ data_structures/nested_lists_store_multiple_data_types.py
@@ -7,4 +7,5 @@ mixed_list = [42, 32.2, 'Alutve', users]
 
 #can call list fns on the nest list as well: 
 user_list =mixed_list.pop()
-print(user_list) # prints [42, 32.2, 'Alutve']
+print(user_list) # prints users ... popped users [] off
+print(mixed_list) # prints [42, 32.2, 'Alutve']
