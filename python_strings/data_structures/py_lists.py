3c9d298cb1e43e713a359cbcbce7963d2b7a531f
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:11:29 -0500

    lists in python

---
+++ python_strings/data_structures/py_lists.py
@@ -0,0 +1,11 @@
+""" notes: array = [list] in python [ ] 
+User DB query - work with lists like ones connected with db to see what gets returned from db query
+John
+Seb
+Ken
+['str','str',.... 'str']
+"""
+
+users = ['John', 'Seb', 'Ken']
+
+print(users) # prints the user with true data structure -- list of str names
