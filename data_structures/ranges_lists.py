735ba051e9f077a5bfa5cbab66529d91d3f8354e
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:49:48 -0500

    : = 0:

--- data_structures/ranges_lists.py
+++ data_structures/ranges_lists.py
@@ -2,6 +2,6 @@
     # list of tags w/ 4 elements
 tags = ['pyton', 'development', 'tutorials', 'code']
 
-tag_range = tags[:2] # stops before tutorials cuz its index 2
-
-print(tag_range) # prints ['python','development'] 
+tag_range = tags[:] # = same as [0:]
+                        # works on both side of the chain
+print(tag_range) # prints all elements
