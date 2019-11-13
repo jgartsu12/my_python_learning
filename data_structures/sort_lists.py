680b5e6a3bb4f3342c28394645fc134b3f586e05
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:20:04 -0500

    numbers and sort()

--- data_structures/sort_lists.py
+++ data_structures/sort_lists.py
@@ -1,4 +1,13 @@
 # How to Sort Lists [] in Python
+
+totals = [231, 1, 23, 342]
+
+print(totals) # list gets printed in same exact order
+
+totals.sort()
+print(totals) # prints them in numeric ascending order --> [1, 23, 231, 342] --- lowest to highest number
+
+
 tags = ['py', 'dvp', 'tutorials', 'code']
 
 print(tags) # prints in exact order of their index what if u want to sort by their alphabetical value
