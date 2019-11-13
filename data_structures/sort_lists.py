618502e131eb8c9b95fc8f841e24266b4a65a519
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:22:40 -0500

    sort in reverse for integers

--- data_structures/sort_lists.py
+++ data_structures/sort_lists.py
@@ -2,17 +2,6 @@
 
 totals = [231, 1, 23, 342]
 
-print(totals) # list gets printed in same exact order
+totals.sort(reverse=True)
 
-totals.sort()
-print(totals) # prints them in integers ascending order --> [1, 23, 231, 342] --- lowest to highest number
-
-
-tags = ['py', 'dvp', 'tutorials', 'code']
-
-print(tags) # prints in exact order of their index what if u want to sort by their alphabetical value
-# lists are muttable
-# use sort() to sort backwards; default with dates sorts oldest firsts and then newests
-tags.sort(reverse=True)
-
-print(tags) # printed ['tutorials', 'py', 'dvp', 'code'] -- list sort(reverse=True) in reverse-alphabetical order
+print(totals) # prints highest to lowest integer in list
