339e5a256296e3a78ea6ac1448095d8b4e64b197
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 08:58:07 -0500

    sqrt fn

--- python_numbers/popular_math_fns.py
+++ python_numbers/popular_math_fns.py
@@ -3,5 +3,5 @@ import math
 loss = -20.25
 product_cost = 89.99
 # math.sqrt fn
-        # sqrt gets u to closes whole number 
+        # sqrt gets square root 
 print(math.sqrt(product_cost)) # prints  9.486305...
