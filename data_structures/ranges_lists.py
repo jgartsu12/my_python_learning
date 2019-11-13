81362df3f40d57586cce383f7e443200408a3f66
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 10:51:34 -0500

    :-1

--- data_structures/ranges_lists.py
+++ data_structures/ranges_lists.py
@@ -2,6 +2,6 @@
     # list of tags w/ 4 elements
 tags = ['pyton', 'development', 'tutorials', 'code']
 
-tag_range = tags[:] # = same as [0:]
+tag_range = tags[:-1] # 'code becomes the deliminator where range stops
                         # works on both side of the chain
-print(tag_range) # prints all elements
+print(tag_range) # prints ['python', 'development', 'tutorials']
