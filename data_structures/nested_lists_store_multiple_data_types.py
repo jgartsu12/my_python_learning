8095f3bef304493091c4fb48f62edd52ec1fce13
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 09:49:37 -0500

    cleaner way of nesting lists

--- data_structures/nested_lists_store_multiple_data_types.py
+++ data_structures/nested_lists_store_multiple_data_types.py
@@ -3,6 +3,6 @@ users = ['K', 'T', 'J', 'L']
 
 ids = [ 1, 2, 3, 4]
 
-mixed_list = [42, 32.2, 'Alutve', ['K', 'T', 'J', 'L']]
+mixed_list = [42, 32.2, 'Alutve', users]
 
-print(mixed_list) # prints mixed list like normal lists with mixed data types
+print(mixed_list) # prints list with nested users variable's list
