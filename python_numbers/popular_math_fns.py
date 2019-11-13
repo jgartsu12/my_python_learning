8740fd8b669532ff6cbfdd1342924021ff4ab8a2
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 08:57:54 -0500

    edit

--- python_numbers/popular_math_fns.py
+++ python_numbers/popular_math_fns.py
@@ -2,6 +2,6 @@ import math
 
 loss = -20.25
 product_cost = 89.99
-# round() fn
-        # round gets u to closes whole number 
-print(round(product_cost)) # prints 90 for product_cost = 89.99 ... prints 89 for 89.12 
+# math.sqrt fn
+        # sqrt gets u to closes whole number 
+print(math.sqrt(product_cost)) # prints  9.486305...
