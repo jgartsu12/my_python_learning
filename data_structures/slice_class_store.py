6553e818bd1f19b69d4ac6de916e30f0b7b21a6b
jgartsu12 <jgartsu12@gmail.com>
2019-11-14 08:50:06 -0500

    what slice() class returns

---
+++ data_structures/slice_class_store.py
@@ -0,0 +1,14 @@
+tags = [
+  'python',
+  'development',
+  'tutorials',
+  'code',
+  'programming',
+]
+
+# print(tags[:2]) # returns ['python', 'development'] --> result set
+# slice class
+slice_obj =slice(2) 
+
+print(slice_obj) # returns obj that gives u access to the result set it proves as an object
+                        # returned --> slice(None, 2, None)
