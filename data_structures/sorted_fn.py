a2d682f5cec68172bc9d46c781e9d3f3704987bb
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 20:30:29 -0500

    how sorted fn changed value of the list made

---
+++ data_structures/sorted_fn.py
@@ -0,0 +1,17 @@
+# Guide to the sorted Function in Python
+# sort list and not change original list
+sale_prices = [
+    100,
+    83,
+    220,
+    40,
+    100,
+    400,
+    10,
+    1,
+    3
+]
+
+sale_prices.sort() # this changesd the entire structure of the items in the list
+
+print(sale_prices) # [1, 3, 10, 40, 83, 100, 220, 400]
