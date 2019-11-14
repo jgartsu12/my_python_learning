95261b6720d15b8ff59e7a70d156c1cfaaaaf745
jgartsu12 <jgartsu12@gmail.com>
2019-11-14 10:13:05 -0500

    more notes

--- data_structures/dictionaries/nested_collections.py
+++ data_structures/dictionaries/nested_collections.py
@@ -1,9 +1,19 @@
 # Guide to Nested Collections in Python Dictionaries
 # key value pair can have collection of values :
+
         # key-value pair can only be 1-1 pair but the value can have a collection like an []
         # helpful-->  break elements to lowest form possible look and dissect into to chunk to know how to work with ea element such as variable, assignment, lists ... dictionaries {key:value}
+                # complex programming = foundational stuff broken down
 teams = {  # teams dictionary  with key being the team, value [players list]
-  "astros": ["Altuve", "Correa", "Bregman"]
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels" : ["Trout","pujols"],
+  "yankees" : ["judge", "stanton"],
 }
 
-print(teams['astros'][0]) #use slice to get return --> Altuve
+astros = teams['astros'] # can store that dictionary in a variable as well
+
+print(astros)
+print(teams['yankees'])
+print(teams['astros'])
+print(teams['angels'])
+
