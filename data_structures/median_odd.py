b74b39c0c07da88de94a337d5bce7f0e21187e4c
jgartsu12 <jgartsu12@gmail.com>
2019-11-13 21:13:04 -0500

    cleaner code

--- data_structures/median_odd.py
+++ data_structures/median_odd.py
@@ -23,13 +23,14 @@ sale_prices = [
 #sorted method to sort lists
 sorted_sales_prices = sorted(sale_prices)
 num_of_sales = len(sorted_sales_prices)
-first_sales_items = sorted_sales_prices[:math.floor(num_of_sales/2)] # first slice to grab first four elements (floor takes 4.5 and provides 4 since floor rounds down)
-last_sales_items = sorted_sales_prices[:-(math.floor(num_of_sales/2)):] # gets last four .... last : brings us all the way to end
-median = sorted_sales_prices[math.floor(num_of_sales/2):(math.floor(num_of_sales/2) + 1)]
+half_slice = math.floor(num_of_sales/2)
+first_sales_items = sorted_sales_prices[:half_slice] # first slice to grab first four elements (floor takes 4.5 and provides 4 since floor rounds down)
+last_sales_items = sorted_sales_prices[:-(half_slice):] # gets last four .... last : brings us all the way to end
+median = sorted_sales_prices[half_slice:(half_slice + 1)]
 
 print(sorted_sales_prices) # prints sorted() list
 print(num_of_sales) # prints 9
 print(first_sales_items) # prints [1, 3, 10, 40]
 print(last_sales_items) # prints [100, 100, 220, 400]
 print(median) # prints [83]
-# code perspective this is very ugly bad practice with duplicate code
+# this is better practice - streamline it store in variable or later on a fucntion and later just call that and have less duplicate code
