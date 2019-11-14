7864c9c53a2606f7f910eaa18411086d18fea1d6
jgartsu12 <jgartsu12@gmail.com>
2019-11-14 10:34:57 -0500

    Guide to Python Dictionary View Objects

---
+++ data_structures/dictionaries/view_obj.py
@@ -0,0 +1,16 @@
+# Guide to Python Dictionary View Objects
+# 
+players = {
+  "ss" : "Correa",
+  "2b" : "Altuve",
+  "3b" : "Bregman",
+  "DH" : "Gattis",
+  "OF" : "Springer",
+}
+
+teams = {
+  "astros" : ["Altuve", "Correa", "Bregman"],
+  "angels":  ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+  "red sox": ["Price", "Betts"],
+}
