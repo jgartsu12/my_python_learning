72c3840f3ed88ac3e3b9a73235a44c49ded50dc8
jgartsu12 <jgartsu12@gmail.com>
2019-11-14 09:07:59 -0500

    slice class 3 arguments exlpained

--- data_structures/slice_class_store.py
+++ data_structures/slice_class_store.py
@@ -1,13 +1,24 @@
 tags = [
-  'python',
+  'python',  # index
   'development',
   'tutorials',
   'code',
   'programming',
 ]
+# explicit approach vs functional approach
+print(tags[1:4:2]) # returns --> ['development', 'code'] --> same as below print statement 
 
-# how to retrieve result set from slice class --> BEHIND THE SCENES
-slice_obj =slice(2)  # difference between this and print(tags[:2]) ==> can store it inside object and call it anywhere in the program allows u store ur slice
-# just doing --> slice_obj = [:2] returns SyntaxError 
+slice_obj =slice(1:4:2)  # like tags[2:4:2]  --> skip every element between index 2 ending at the 4th index element
+                        # with slice u could start with second element which has an index  of one to last element with index of 5
+
+print(slice_obj) # returns slice(None, 2, None)
+                        # explaination of these 3 arguments returned in slice:
+                       
+                        #       1. Start point
+                        #       2. Step
+                        #       3. End point 
 
 print(tags[slice_obj]) # --> this retrieves ['python', 'development'] result set previously obtained in print(slice_obj)
+                        # returns --> ['development', 'code'] which is the same as print(tags[1:4:2])
+
+# an alg may return a slice but u may not know the start or stop
