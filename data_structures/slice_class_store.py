2e49525746c61169d6ece480fd04ae28cf600db6
jgartsu12 <jgartsu12@gmail.com>
2019-11-14 08:56:16 -0500

    # how to retrieve result set from slice class --> BEHIND THE SCENES

--- data_structures/slice_class_store.py
+++ data_structures/slice_class_store.py
@@ -6,7 +6,8 @@ tags = [
   'programming',
 ]
 
-# how to retrieve result set from slice class
-slice_obj =slice(2) 
+# how to retrieve result set from slice class --> BEHIND THE SCENES
+slice_obj =slice(2)  # difference between this and print(tags[:2]) ==> can store it inside object and call it anywhere in the program allows u store ur slice
+# just doing --> slice_obj = [:2] returns SyntaxError 
 
 print(tags[slice_obj]) # --> this retrieves ['python', 'development'] result set previously obtained in print(slice_obj)
