0896b0cc45884ad2a2bf862ce3d868109948c4fe
jgartsu12 <jgartsu12@gmail.com>
2019-11-14 09:44:28 -0500

    # how can we store it in new variable

--- data_structures/add_to_list.py
+++ data_structures/add_to_list.py
@@ -3,26 +3,13 @@ tags = ['python', 'development', 'tutorials', 'code']
 # algorithms when iterating over a list makes u add elements to the list while looping through it
 # key differences between In Place vs Copy
 
-# wrong way to add to a list
-tags[-1] = 'Programming'
-
-tags.extend('programming') # returns --> ['python', 'development', 'tutorials', 'Programming'] # code got replaced with programming
-                    # seems liked it worked BUTTTT --> ur not adding to list instead it got rid of the last element 'code' so it override it instead!!
-
-#extend() --> take each string element and treated it as its own character -->
-        # extend is only used to add to prexisting list
-tags.extend('programming') # returned ==> ['python', 'development', 'tutorials', 'code', 'p','r', 'o', 'g', 'r', 'a', 'm', 'i', 'n', 'g']
-
-# wrap string in ['str' ] to treat the str as a single element  # MOST COMMON PRACTICE ### could also wrap in data structure
-tags.extend(['programming']) # --> returned : ['python', 'code', 'development', 'tutorials', 'Programming']
-
-print(tags)
-
-# trying to store in a new variable --> similar to sort() fn
-new_tags = tags.extend(['programming'])
-
-print(new_tags) # returns None --> extend changes tags in place but does not return a new value stored similar to sort() fn beh
-
 # how can we store it in new variable
+# THIS IS NOT IN PLACE and does not alter the first variable with list stored in it
+new_tags = tags + ['Programming'] ## adds programming tot he end of the previous list --> must be wrapped in ['str']
 
-new_tags = tags + ['Programming']
+# cant do:
+new_tags = tags + 'Programming' # - cant add a string to a list since they are diff data types --> str vs ['list']
+
+print(new_tags) # --> returns ['python', 'development', 'tutorials', 'code', 'Programming']
+
+print(tags) # --> returns  ['python', 'development', 'tutorials', 'code'] still 
