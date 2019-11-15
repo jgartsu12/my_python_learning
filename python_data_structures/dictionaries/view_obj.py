5ebb3edef17ae640a9230bc16e0360b21fec1ef6
jgartsu12 <jgartsu12@gmail.com>
2019-11-15 10:41:16 -0500

    nested colelction with view objs

--- python_data_structures/dictionaries/view_obj.py
+++ python_data_structures/dictionaries/view_obj.py
@@ -1,22 +1,33 @@
 # Guide to Python Dictionary View Objects
-# view obj -->  thread safety
-players = {
-  "ss" : "Correa", 
-  "2b" : "Altuve",
-  "3b" : "Bregman",
-  "DH" : "Gattis",
-  "OF" : "Springer",
-}
 
-# thread safety - thread safe (senior-lvl-dvp) make quick copy of list and then perform our actions
-# use of copy() fn --> perform any actions u want without data change
-player_names = list(players.copy().values())
-
-print(player_names) # prints players {} values aka name of the players
-                        # only we can access these players stored
+#nested collections in dictionary
 teams = {
   "astros" : ["Altuve", "Correa", "Bregman"],
   "angels":  ["Trout", "Pujols"],
   "yankees": ["Judge", "Stanton"],
   "red sox": ["Price", "Betts"],
 }
+
+team_groupings = teams.items()
+
+print(team_groupings) # --> returns dicitonary view objt with tuples
+                      # returns the tuple items --> certain fns can be performed on these per doc
+
+
+
+# previous notes
+# # view obj -->  thread safety
+# players = {
+#   "ss" : "Correa", 
+#   "2b" : "Altuve",
+#   "3b" : "Bregman",
+#   "DH" : "Gattis",
+#   "OF" : "Springer",
+# }
+
+# # thread safety - thread safe (senior-lvl-dvp) make quick copy of list and then perform our actions
+# # use of copy() fn --> perform any actions u want without data change
+# player_names = list(players.copy().values())
+
+# print(player_names) # prints players {} values aka name of the players
+#                         # only we can access these players stored
