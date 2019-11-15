538a815ca09824bd12a1b16479726ae3c7a6be19
jgartsu12 <jgartsu12@gmail.com>
2019-11-15 11:09:59 -0500

    lists of nested dictionaries

---
+++ python_data_structures/dictionaries/lists_of_nested_dictionaries.py
@@ -0,0 +1,20 @@
+# Guide to Working with Lists of Nested Dictionaries
+
+teams = [
+  {
+    'astros': {
+      '2B': 'Altuve',
+      'SS': 'Correa',
+      '3B': 'Bregman',
+    }
+  },
+  {
+    'angels': {
+      'OF': 'Trout',
+      'DH': 'Pujols',
+    }
+  }
+]
+
+
+
