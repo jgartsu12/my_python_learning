eb574e85cc3b826fff699a15a8c1b08826c19da3
jgartsu12 <jgartsu12@gmail.com>
2019-11-15 11:16:58 -0500

    traversing the nest

--- python_data_structures/dictionaries/lists_of_nested_dictionaries.py
+++ python_data_structures/dictionaries/lists_of_nested_dictionaries.py
@@ -1,20 +1,24 @@
 # Guide to Working with Lists of Nested Dictionaries
-
-teams = [
-  {
-    'astros': {
-      '2B': 'Altuve',
+# how one collection can store another collection (web mobile apps; machine learning ...)
+# teams db: list w/ various dictionariies
+# use  case studies to test this out and get used to this to work with API data and db queries!! TO DO (**(*))
+teams = [ # list of
+  {   # dictionary 1
+    'astros': { # key : value         # index 0
+      '2B': 'Altuve', # elements
       'SS': 'Correa',
       '3B': 'Bregman',
     }
   },
-  {
-    'angels': {
+  {   # dicitionary 2
+    'angels': {           # index 1
       'OF': 'Trout',
       'DH': 'Pujols',
     }
   }
 ]
+
+print(teams[0]) # prints astros dictionary
 
 
 
