cbb5c2d39c10228f1d970473de2babf8eac9464c
jgartsu12 <jgartsu12@gmail.com>
2019-11-17 09:10:39 -0500

    loop characters in a str notes

---
+++ python_loops/characters_in_string.py
@@ -0,0 +1,13 @@
+# How to Loop Through the Characters of a Python String
+alphebet = 'abcdef'  # string is really a collection of characters
+
+for letter in alphebet:
+    print(letter) 
+   ''' returns:
+        a
+        b
+        c
+        d
+        e
+        f
+    '''
