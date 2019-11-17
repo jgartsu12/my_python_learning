04c8cf07b7ada5b14ea28422994fc25d935e24ba
jgartsu12 <jgartsu12@gmail.com>
2019-11-17 11:03:12 -0500

    tern op vs conditional can access

---
+++ python_conditionals/ternary_operators.py
@@ -0,0 +1,24 @@
+# How to Use the Ternary Operator in Python Conditionals
+# need to be careful where  u use these ... PEP 20 3rd item simple is better than complex
+# if ternary op can read like normal language then its good time to use it 
+# reorganzies  a typical conditional works for us
+
+role = 'admin'
+
+# auth = 'can access' if role == 'admin' else 'cannot access'
+
+# print(auth) # => can access
+# ~~~~~~~~~~~~~~~
+# role = 'guest'
+
+# auth = 'can access' if role == 'admin' else 'cannot access'
+
+# print(auth) # => cannot access
+
+# ~~~~ comparision of ternary op to conditional
+if role == 'admin':
+    auth = 'can access'
+else:
+    auth = 'cannot access'
+
+print(auth) # prints cann access
