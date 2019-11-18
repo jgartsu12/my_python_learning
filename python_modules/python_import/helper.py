e4ff2b9eb9451641ee1b5f44ae779373da50b659
jgartsu12 <jgartsu12@gmail.com>
2019-11-18 10:22:05 -0500

    created repl with custom module

---
+++ python_modules/python_import/helper.py
@@ -0,0 +1,16 @@
+# def greeting(first, last):
+#     return f'Hi {first} {last}'
+
+
+# print(greeting('l', 'j')) # returns => Hi l j
+
+# how to import greeting directly into the repl so it can be returned in the terminal
+def greeting(first, last):
+    return f'Hi {first} {last}'
+# in terminal:
+'''
+python
+import helper
+helper.greeting('J', 'Low')
+'Hi J Low'
+''' 
