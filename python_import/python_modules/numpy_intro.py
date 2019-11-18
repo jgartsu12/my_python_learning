5d58fb12b38bb442b7bad07d3115dba8fa3204ee
jgartsu12 <jgartsu12@gmail.com>
2019-11-18 11:23:10 -0500

    numpy

---
+++ python_import/python_modules/numpy_intro.py
@@ -0,0 +1,14 @@
+# Introduction to the Numpy Package in Python
+import numpy as np
+num_range = np.arange(16)
+num_range                               9, 10, 11, 12, 13, 14,
+''' => array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,
+9, 10, 11, 12, 13, 14, 15])
+'''
+
+num_range.reshape(4, 4)
+''' => array([[ 0,  1,  2,  3],
+       [ 4,  5,  6,  7],
+       [ 8,  9, 10, 11],
+       [12, 13, 14, 15]])
+       '''
--- python_modules/python_import/main.py
+++
@@ -1,9 +0,0 @@
-import sys # directly in py lang
-sys.path.insert(0, './libs') # look into working directly to find libs
-import helper
-
-def render():
-    print(helper.greeting('T', 'hhh'))
-
-
-render()
