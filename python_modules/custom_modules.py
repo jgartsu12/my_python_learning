782918af990aad435cb2422edfdb757e4ecf415a
jgartsu12 <jgartsu12@gmail.com>
2019-11-18 10:14:33 -0500

    notes

---
+++ python_modules/custom_modules.py
@@ -0,0 +1,20 @@
+# How to Create a Custom Module and Import It In the Python Repl
+# started in terminal
+# type python in terminal to open up repl
+# write import libary_name
+'''  
+# PS C:\Users\14pythonsktop\python_learning\python_modules>
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC
+v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+# >>> import math
+# >>> math.sqrt(25)
+5.0
+'''
+# exit py repl via exit()
+''' custom libary 
+terminal:
+mkdir python_import
+cd python_import 
+~~~ 
+'''
